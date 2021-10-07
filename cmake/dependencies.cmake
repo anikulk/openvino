27d00f671c71022e3d9ae77cbcf57cb71dcc6ece
Anisha Kulkarni <anisha.dattatraya.kulkarni@intel.com>
2021-10-07 16:40:00 -0700

    Enable CHROMEOS support for GNAPlugin
    - Avoid downloading libgna, as we build from source
    - Modify install location for libGNAPlugin and
      dependencies
    
    Signed-off-by: Anisha Kulkarni <anisha.dattatraya.kulkarni@intel.com>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -255,20 +255,22 @@ if(ENABLE_GNA)
             GNA_LIB_DIR
             libGNA_INCLUDE_DIRS
             libGNA_LIBRARIES_BASE_PATH)
-    if(GNA_LIBRARY_VERSION STREQUAL "GNA1")
-        RESOLVE_DEPENDENCY(GNA
-                ARCHIVE_UNIFIED "GNA/gna_20181120.zip"
-                TARGET_PATH "${TEMP}/gna"
-                SHA256 "b631d6cc5f6cca4a89a3f5dfa383066f3282fee25d633d9085c605bdd8090210")
-    else()
-        if(GNA_LIBRARY_VERSION STREQUAL "GNA1_1401")
-            set(GNA_VERSION "01.00.00.1401")
-            set(GNA_HASH "cc954e67525006bf8bd353a6682e38bf208f6d74e973e0fc292850e721f17452")
-        endif()
-        if(GNA_LIBRARY_VERSION STREQUAL "GNA2")
-            set(GNA_VERSION "03.00.00.1377")
-            set(GNA_HASH "d45fb48994d8c2803a16e88e29ae48851066325b97c1c6c4a5bf4f4573d55c65")
-        endif()
+    if(NOT LINUX_OS_NAME STREQUAL "CHROMEOS")
+	             if(GNA_LIBRARY_VERSION STREQUAL "GNA1")
+	                 RESOLVE_DEPENDENCY(GNA
+	                         ARCHIVE_UNIFIED "GNA/gna_20181120.zip"
+	                         TARGET_PATH "${TEMP}/gna"
+	                         SHA256 "b631d6cc5f6cca4a89a3f5dfa383066f3282fee25d633d9085c605bdd8090210")
+             else()
+                 if(GNA_LIBRARY_VERSION STREQUAL "GNA1_1401")
+                     set(GNA_VERSION "01.00.00.1401")
+                     set(GNA_HASH "cc954e67525006bf8bd353a6682e38bf208f6d74e973e0fc292850e721f17452")
+                 endif()
+                 if(GNA_LIBRARY_VERSION STREQUAL "GNA2")
+                     set(GNA_VERSION "02.00.00.1226")
+                     set(GNA_HASH "d5450af15c993e264c25ac4591a7dab44722e10d15fca4f222a1b84429d4e5b6")
+                 endif()
+    endif()
 
         set(FILES_TO_EXTRACT_LIST gna_${GNA_VERSION}/include)
         if(WIN32)
@@ -277,12 +279,14 @@ if(ENABLE_GNA)
             LIST(APPEND FILES_TO_EXTRACT_LIST gna_${GNA_VERSION}/linux)
         endif()
      
+    if(NOT LINUX_OS_NAME STREQUAL "CHROMEOS")
         RESOLVE_DEPENDENCY(GNA
                 ARCHIVE_UNIFIED "GNA/GNA_${GNA_VERSION}.zip"
                 TARGET_PATH "${TEMP}/gna_${GNA_VERSION}"
                 VERSION_REGEX ".*_([0-9]+.[0-9]+.[0-9]+.[0-9]+).*"
                 FILES_TO_EXTRACT FILES_TO_EXTRACT_LIST
                 SHA256 ${GNA_HASH})
+    endif()
     endif()
     update_deps_cache(GNA "${GNA}" "Path to GNA root folder")
     debug_message(STATUS "gna=" ${GNA})
